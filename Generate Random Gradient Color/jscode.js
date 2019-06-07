1b846a624d75638c47f3b1e55d9909d849181778
Shashank Sharma <35090897+geekyorion@users.noreply.github.com>
2019-06-07 14:04:32 +0530

    Resolution increased

--- Generate Random Gradient Color/jscode.js
+++ Generate Random Gradient Color/jscode.js
@@ -143,7 +143,7 @@ window.onload = function() {
   });
   
   resolution_square.addEventListener("click", ()=>{
-    var canvas = createCanvas(1000, 1000);
+    var canvas = createCanvas(5000, 5000);
     var dataURL = canvas.toDataURL('image/png');
     resolution_square.href = dataURL;
   });
